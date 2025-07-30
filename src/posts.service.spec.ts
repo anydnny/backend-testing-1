6d4ea37e9037ab3f28ebd277fde8ce5d17ac2e7e
Daniil Glushakov <anydnny@gmail.com>
2025-07-30 21:43:25 +0300

    test

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -1,22 +1,24 @@
 import { Post, PostsService } from './posts.service';
 
-describe('PostsService', () => {
+describe("PostsService", () => {
   let postsService: PostsService;
-  const post: Omit<Post, 'id' | 'date'> = {
-    text: 'Mocked post',
+  const post: Omit<Post, "id" | "date"> = {
+    text: "Mocked post",
   };
 
   beforeEach(async () => {
     postsService = new PostsService();
 
-    postsService.create({ text: 'Some pre-existing post' });
+    postsService.create({ text: "Some pre-existing post" });
   });
 
-  it('should add a new post', () => {
+  it("should add a new post", () => {
     // реализуйте тест-кейс
+    postsService.create(post);
+    expect(postsService.find("2")?.text).toEqual(post.text);
   });
-
-  it('should find a post', () => {
+  it("should find a post", () => {
     // реализуйте тест-кейс
+    expect(postsService.find("1")?.text).toEqual("Some pre-existing post");
   });
 });
